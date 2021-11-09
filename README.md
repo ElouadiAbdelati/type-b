00adab86e1f704f2492256cf3385fe8923ede17d
imaneEmi <imane.elaimani@gmail.com>
2021-11-09 10:55:01 +0100

    Adding admin edit form

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # type_b
 Projet présidence-Formulaire type B
+
+# to generate database tables please run
+php artisan migrate
+
+# once the tables are created comment the following line when found in the migration files
+Schema::disableForeignKeyConstraints();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin_edit_form', function () {
+    return view('admin_edit_demande');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
